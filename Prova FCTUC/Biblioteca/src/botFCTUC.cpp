5827c988e29d39929b7bf3b45aa58713daa805e6
JNDVasco <dev.jndvasco@protonmail.com>
2022-04-14 23:33:14 +0100

    New Stuff

--- Prova FCTUC/Biblioteca/src/botFCTUC.cpp
+++ Prova FCTUC/Biblioteca/src/botFCTUC.cpp
@@ -4,6 +4,13 @@
 // =
 // =============================================================
 #include "botFCTUC.h"
+#include <Arduino.h>
+
+/*!
+  @brief
+  @param
+  @return
+*/
 
 // =============================================================
 // = Class Constructor and Destructor ==========================
@@ -12,19 +19,12 @@
 // Constructor
 botFCTUC::botFCTUC()
 {
-    pinMode(motor1Dir, OUTPUT);
-    pinMode(motor1PWM, OUTPUT);
-    pinMode(motor2PWM, OUTPUT);
-    pinMode(neopixel, OUTPUT);
-    pinMode(motor2Dir, OUTPUT);
-    pinMode(xshutRight, OUTPUT);
-    pinMode(xshutFront, OUTPUT);
-    pinMode(xhsutLeft, OUTPUT);
-    pinMode(flameSensor, INPUT);
-    pinMode(buttonPin, INPUT);
-
-    // Setup Fan
-    fanSetup();
+  // Setup Fan
+  fanSetup();
+  // Setup Button
+  buttonSetup();
+  // Setup Buzzer
+  buzzerSetup();
 
 } // End Constructor
 
@@ -37,50 +37,206 @@ botFCTUC::~botFCTUC()
 // = Fan Control Funtions ======================================
 // =============================================================
 
+/*!
+  @brief   Set up the Fan
+*/
 void botFCTUC::fanSetup()
 {
-    pinMode(fanPin, OUTPUT);
-    fanOff();
+  pinMode(_fanPin, OUTPUT);
+  fanOff();
 } // End fanSetup
 
-// Turns fan on (Logic is Inverted!!)
+/*!
+  @brief Turn the fan on
+  @note Control Logic is Inverted (1 = Off, 0 = On)
+*/
 void botFCTUC::fanOn()
 {
-    digitalWrite(fanPin, LOW);
+  digitalWrite(_fanPin, LOW);
 } // End fanOn
 
-// Turns fan off (Logic is Inverted!!)
+/*!
+  @brief Turn the fan off
+  @note Control Logic is Inverted (1 = Off, 0 = On)
+*/
 void botFCTUC::fanOff()
 {
-    digitalWrite(fanPin, HIGH);
+  digitalWrite(_fanPin, HIGH);
 } // End fanOff
 
+// =============================================================
+// = Button Control Funtions ===================================
+// =============================================================
+
+/*!
+  @brief   Set up the Button
+*/
 void botFCTUC::buttonSetup()
 {
-    pinMode(buttonPin, INPUT);
+  pinMode(_buttonPin, INPUT);
 }
 
+/*!
+  @brief   Waits in a infinite loop until the button is pressed to exit
+*/
 void botFCTUC::waitStart()
 {
-    while (!digitalRead(buttonPin))
-    {
-    }
+  DEBUG_PRINTLN("[INFO] - BotFCTUC Waiting to start!");
 
-    Serial.println("Starting Program!");
+  while (!digitalRead(_buttonPin))
+  {
+    if (millis() - _previousMillis >= 500)
+    {
+      _previousMillis = millis();
+      this->pixelSetColor(0, !(greenOrBlue)*255, (greenOrBlue)*255);
+      greenOrBlue = !greenOrBlue;
+    }
+  }
+
+  DEBUG_PRINTLN("[INFO] - BotFCTUC Is starting!");
 }
 
+/*!
+  @brief   Read the button status
+  @return  The button Status
+*/
 bool botFCTUC::readButton()
 {
-    return digitalRead(buttonPin);
+  return digitalRead(_buttonPin);
 }
 
-void setupBuzzer()
+// =============================================================
+// = Buzzer Control Funtions ===================================
+// =============================================================
+/*!
+  @brief   Set up the Buzzer
+*/
+void botFCTUC::buzzerSetup()
 {
-    pinMode(buzzerPin, OUTPUT);
-    buzzer_tone(0);
+  pinMode(_buzzerPin, OUTPUT);
+  buzzerPlay(0);
+}
+/*!
+  @brief   Play a sound on the buzzer
+  @param   Val Tone to be played
+*/
+void botFCTUC::buzzerPlay(uint8_t val)
+{
+  analogWrite(_buzzerPin, val);
 }
 
-void buzzerPlay(int8_t val)
+// =============================================================
+// = NeoPixel Control Funtions =================================
+// =============================================================
+/*!
+  @brief   Set up the neopixel
+*/
+void botFCTUC::neopixelSetup()
 {
-    analogWrite(buzzerPin, val);
+  pinMode(_neopixelPin, OUTPUT);
+
+  // Create Neopixel and start it
+  this->_pixel = new Adafruit_NeoPixel(1, _neopixelPin,
+                                       NEO_GRB + NEO_KHZ800);
+  this->_pixel->begin();
+  pixelSetBrightness(255);
+  pixelSetColor(255, 255, 255);
+  // DEBUG_PRINTLN("[INFO] - NeoPixel Started");
+}
+
+/*!
+  @brief   Set the color to a given R G & B Values
+  @param   Red Red Amount 0 -> 255
+  @param   Green Red Amount 0 -> 255
+  @param   Blue Red Amount 0 -> 255
+*/
+void botFCTUC::pixelSetColor(uint8_t Red, uint8_t Green, uint8_t Blue)
+{
+  // Prevent the user from being funny and messing with me
+  constrain(Red, 0, 255);
+  constrain(Green, 0, 255);
+  constrain(Blue, 0, 255);
+
+  this->_pixel->setPixelColor(0, this->_pixel->Color(Red, Green, Blue)); // (pixelID, cor)
+  this->_pixel->show();
+}
+/*!
+  @brief   Set the color to a given R G & B Values
+  @param   Brightness Brightness Amount 0 -> 255
+*/
+void botFCTUC::pixelSetBrightness(uint8_t Brightness)
+{
+  // Prevent the user from being funny and messing with me
+  constrain(Brightness, 0, 255);
+  this->_pixel->setBrightness(Brightness);
+}
+// =============================================================
+// = Flame Sensor Control Funtions =============================
+// =============================================================
+/*!
+  @brief   Set up the flame sensor
+*/
+void botFCTUC::flameSetup()
+{
+  pinMode(_flameSensor, INPUT);
+}
+
+/*!
+  @brief   Get the sensor value from the flame sensor
+  @return  The flame sensor value 0 -> 1023
+*/
+uint16_t botFCTUC::getFlame()
+{
+  return analogRead(_flameSensor);
+}
+
+/*!
+  @brief   Print the current value of the flame sensor
+*/
+void botFCTUC::printFlame()
+{
+  DEBUG_PRINTLN("Flame Sensor: " + String(getFlame()));
+}
+
+// =============================================================
+// = Color Sensor Control Funtions =============================
+// =============================================================
+/*!
+  @brief   Set up the color sensor
+  @return  True if sucessful, false if not setup
+*/
+bool botFCTUC::setupColorSensor()
+{
+  _colorSensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_1X);
+  return _colorSensor.begin();
+}
+
+/*!
+ *  @brief  Return the Red, Green, Blue, Clear values
+ *  @param  RGBC[0]
+ *          Red value
+ *  @param  RGBC[1]
+ *          Green value
+ *  @param  RGBC[2]
+ *          Blue value
+ *  @param  RGBC[3]
+ *          Clear channel value
+ */
+void botFCTUC::getColor(uint16_t RGBC[])
+{
+  _colorSensor.getRawData(&RGBC[0], &RGBC[1], &RGBC[2], &RGBC[3]);
+}
+/*!
+ *  @brief Enable the color Sensor
+ */
+void botFCTUC::enableColorSensor()
+{
+  _colorSensor.enable();
+}
+/*!
+ *  @brief Disable the color Sensor
+ */
+void botFCTUC::disableColorSensor()
+{
+  _colorSensor.disable();
 }
--- Prova FCTUC/Biblioteca/src/botFCTUC.h
+++ Prova FCTUC/Biblioteca/src/botFCTUC.h
@@ -2,28 +2,92 @@
 // = Header File botFTCUC                       BotOlympics 2022
 // = JNDVasco - Rev 1.0
 // =
+// =
+// = Hardware Mapping
+// = |---------------------|--------|
+// = |     Descrição       |  Pino  |
+// = |---------------------|--------|
+// = |Motor 1 - Dir        |    4   |
+// = |---------------------|--------|
+// = |Motor 1 - PWM        |    5   |
+// = |---------------------|--------|
+// = |Motor 2 - PWM        |    6   |
+// = |---------------------|--------|
+// = |LED NeoPixel         |    7   |
+// = |---------------------|--------|
+// = |Motor 2 - Dir        |    8   |
+// = |---------------------|--------|
+// = |Buzzer               |    9   |
+// = |---------------------|--------|
+// = |XSHUT LiDAR Direita  |   10   |
+// = |---------------------|--------|
+// = |XSHUT LiDAR Frente   |   11   |
+// = |---------------------|--------|
+// = |XSHUT LiDAR Esquerda |   12   |
+// = |---------------------|--------|
+// = |Sensor infravermelhos|   A0   |
+// = |---------------------|--------|
+// = |Botão                |   A1   |
+// = |---------------------|--------|
+// = |Ventoinha            |   A2   |
+// = |---------------------|--------|
+// =
+// =
 // =============================================================
 
-// Guard Clauses
-#ifndef __botFCTUC
-#define __botFCTUC
+// Header guards
+#ifndef BOT_FCTUC_H
+#define BOT_FCTUC_H
 
-#define motor1Dir 4    // Motor 1 - Dir
-#define motor1PWM 5    // Motor 1 - PWM
-#define motor2PWM 6    // Motor 2 - PWM
-#define neopixel 7     // LED NeoPixel
-#define motor2Dir 8    // Motor 2 - Dir
-#define buzzerPin 9    // Buzzer
-#define xshutRight 10  // XSHUT LiDAR Direita
-#define xshutFront 11  // XSHUT LiDAR Frente
-#define xhsutLeft 12   // XSHUT LiDAR Esquerda
-#define flameSensor A0 // Sensor infravermelhos
-#define buttonPin A1   // Botão
-#define fanPin A2      // Ventoinha
+//----------------------------------------------------------
+// Coment this line to disable the serial print
+#define DEBUG
+
+// This provides an easy way to debug and remove the
+// Serial Prints Later
+#ifdef DEBUG
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINTLN(x) Serial.println(x)
+#else
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
+#endif
+
+//----------------------------------------------------------
+// External Auxiliry Libraries
+#include <./external/Adafruit_NeoPixel.h>
+#include <./external/Adafruit_TCS34725.h>
+#include <./external/VL53L0X.h>
+//----------------------------------------------------------
 
 class botFCTUC
 {
 private:
+    // Pin Declarations
+    const uint8_t _motor1Dir = 4;    // Motor 1 - Dir
+    const uint8_t _motor1PWM = 5;    // Motor 1 - PWM
+    const uint8_t _motor2PWM = 6;    // Motor 2 - PWM
+    const uint8_t _neopixelPin = 7;  // LED NeoPixel
+    const uint8_t _motor2Dir = 8;    // Motor 2 - Dir
+    const uint8_t _buzzerPin = 9;    // Buzzer
+    const uint8_t _xshutRight = 10;  // XSHUT LiDAR Direita
+    const uint8_t _xshutFront = 11;  // XSHUT LiDAR Frente
+    const uint8_t _xhsutLeft = 12;   // XSHUT LiDAR Esquerda
+    const uint8_t _flameSensor = A0; // Sensor infravermelho
+    const uint8_t _buttonPin = A1;   // Botão
+    const uint8_t _fanPin = A2;      // Ventoinha
+    //----------------------------------------------------------
+    // Neopixel Led
+    Adafruit_NeoPixel *_pixel;
+    //----------------------------------------------------------
+    // Some Vars
+    uint32_t _previousMillis = 0;
+    bool greenOrBlue = false;
+    //----------------------------------------------------------
+    // Color Sensor
+    Adafruit_TCS34725 _colorSensor;
+    //----------------------------------------------------------
+
 public:
     botFCTUC();
     ~botFCTUC();
@@ -42,7 +106,23 @@ public:
     bool readButton();
 
     // Buzzer Routines
-    void setupBuzzer();
-    void buzzer_tone(int valor);
+    void buzzerSetup();
+    void buzzerPlay(uint8_t val);
+
+    // NeoPixel Routines
+    void neopixelSetup();
+    void pixelSetColor(uint8_t Red, uint8_t Green, uint8_t Blue);
+    void pixelSetBrightness(uint8_t Brightness);
+
+    // Flame Sensor
+    void flameSetup();
+    uint16_t getFlame();
+    void printFlame();
+
+    // Color Sensor
+    bool setupColorSensor();
+    void getColor(uint16_t RGBC[]);
+    void enableColorSensor();
+    void disableColorSensor();
 };
 #endif
